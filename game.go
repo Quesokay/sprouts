40fdbf55ed8ea165a64a75cefd075aa41ccabca6
Victor <ques4235@gmail.com>
2026-06-02 15:03:52 +0200

    Basic character movement.

--- game.go
+++ game.go
@@ -2,18 +2,75 @@ package main
 
 import rl "github.com/gen2brain/raylib-go/raylib"
 
-func main() {
-  rl.InitWindow(800, 450, "raylib [core] example - basic window")
-  defer rl.CloseWindow()
+const(
+  screenWidth = 1000
+  screenHeight = 480
+)
 
+var(
+  running = true
+  bkgColor = rl.NewColor(147,211,196,255)
+
+  grassSprite rl.Texture2D
+  playerSprite rl.Texture2D
+
+  playerSrc rl.Rectangle
+  playerDest rl.Rectangle
+
+  playerSpeed float32 = 3
+)
+
+func drawScene(){
+  rl.DrawTexture(grassSprite, 100, 50, rl.White)
+  rl.DrawTexturePro(playerSprite, playerSrc, playerDest, rl.NewVector2(playerDest.Width, playerDest.Height), 0, rl.White)
+}
+func input(){
+  if rl.IsKeyDown(rl.KeyW) || rl.IsKeyDown(rl.KeyUp){
+    playerDest.Y -= playerSpeed
+  }
+  if rl.IsKeyDown(rl.KeyS) || rl.IsKeyDown(rl.KeyDown){
+    playerDest.Y += playerSpeed
+  }
+  if rl.IsKeyDown(rl.KeyA) || rl.IsKeyDown(rl.KeyLeft){
+    playerDest.X -= playerSpeed
+  }
+  if rl.IsKeyDown(rl.KeyD) || rl.IsKeyDown(rl.KeyRight){
+    playerDest.X += playerSpeed
+  }
+}
+func update(){
+  running = !rl.WindowShouldClose()
+}
+func render(){
+  rl.BeginDrawing()
+  rl.ClearBackground(bkgColor)
+  drawScene()
+
+  rl.EndDrawing()
+}
+
+func init(){
+  rl.InitWindow(screenWidth, screenHeight, "Sproutlings")
+  rl.SetExitKey(0)
   rl.SetTargetFPS(60)
 
-  for !rl.WindowShouldClose() {
-    rl.BeginDrawing()
+  grassSprite = rl.LoadTexture("res/Tilesets/Grass.png")
+  playerSprite = rl.LoadTexture("res/Characters/BasicCharakterSpritesheet.png")
 
-    rl.ClearBackground(rl.RayWhite)
-    rl.DrawText("Congrats! You created your first window!", 190, 200, 20, rl.LightGray)
+  playerSrc = rl.NewRectangle(0, 0, 48, 48)
+  playerDest =rl.NewRectangle(200, 200, 100, 100)
+}
+func quit(){
+  rl.UnloadTexture(grassSprite)
+  rl.UnloadTexture(playerSprite)
+  defer rl.CloseWindow()
+}
 
-    rl.EndDrawing()
+func main() {
+
+  for running {
+    input()
+    update()
+    render()
   }
 }
